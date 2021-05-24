02c6c8e9f33f9506fea77b52fe910a8a72742da4
Yurii Khomin <yurii.khomin@gmail.com>
2021-05-24 05:44:12 +0300

    fix function

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,7 +2,7 @@ const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 const fetchCountries = searchQuery => {
     return fetch(`${BASE_URL}/name/${searchQuery}`)
-    .then(response => response.json(),)
+    .then(response => response.json())
 }
 
 export default { fetchCountries };
